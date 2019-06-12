b94546013d04ac66155c08b97dc64ea12e33e2d1
League Student <league@WorthyMini-6.attlocal.net>
2019-06-11 18:27:20 -0700

    :smile:

--- discordBotExample/src/main/java/command_classes/SolveQuadraticListener.java
+++ discordBotExample/src/main/java/command_classes/SolveQuadraticListener.java
@@ -1,5 +1,7 @@
 package command_classes;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
 import java.time.Instant;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -15,10 +17,14 @@ public class SolveQuadraticListener extends CustomMessageCreateListener {
 	private static int stage = 0;
 	private static String n = "";
 	private User user;
-	private static double a = 0;
-	private static double b = 0;
-	private static double c = 0;
-	
+	private static MathContext mc = new MathContext(9);
+	private static BigDecimal a;
+	private static BigDecimal b;
+	private static BigDecimal c;
+	private static BigDecimal ans1;
+	private static BigDecimal ans2;
+	private static BigDecimal four = new BigDecimal(4,mc);
+	private static BigDecimal two = new BigDecimal(2,mc);
 
 	public SolveQuadraticListener(String channelName) {
 		super(channelName);
@@ -36,23 +42,35 @@ public class SolveQuadraticListener extends CustomMessageCreateListener {
 		else if(stage == 1) {
 			if(event.getMessageAuthor().getDisplayName().equals(n)) {
 				stage = 2;
-				a = Double.parseDouble(event.getMessageContent());
+				a = new BigDecimal(event.getMessageContent(),mc);
 				event.getChannel().sendMessage("Enter the 'B' Value");
 			}
 		}
 		else if(stage == 2) {
 			if(event.getMessageAuthor().getDisplayName().equals(n)) {
 				stage = 3;
-				b = Double.parseDouble(event.getMessageContent());
+				b = new BigDecimal(event.getMessageContent(),mc);
 				event.getChannel().sendMessage("Enter the 'C' Value");
 			}
 		}
 		else if(stage == 3) {
 			if(event.getMessageAuthor().getDisplayName().equals(n)) {
 				stage = 0;
-				c = Double.parseDouble(event.getMessageContent());
-				double a1 = ((-b) + Math.sqrt(Math.pow(b, 2.0) - (4*a*c))/(2*a));
-				event.getChannel().sendMessage("" + a1);
+				c = new BigDecimal(event.getMessageContent(),mc);
+				try {
+					BigDecimal bottom = two.multiply(a);
+					BigDecimal desc = new BigDecimal(Math.sqrt(((b.pow(2).subtract(four.multiply(a).multiply(c))).doubleValue())));
+					BigDecimal negb = b.negate();
+					ans1 = (negb.add(desc)).divide(bottom);
+					ans2 = (negb.subtract(desc)).divide(bottom);
+					if(ans1.equals(ans2)) {
+						event.getChannel().sendMessage("Your answer is " + ans1.floatValue());
+					} else {
+						event.getChannel().sendMessage("Your answers are " + ans1.floatValue() + " and " + ans2.floatValue());
+					}
+				} catch(Exception e) {
+					event.getChannel().sendMessage("Your answer is " + e.getMessage());
+				}
 			}
 		}
 	}
